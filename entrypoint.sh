c6bd0a72b14dfaf76ad6a070029c8b4592d03eea
Daniele E. Domenichelli <daniele.domenichelli@gmail.com>
2020-02-12 20:51:01 +0100

    [Refactor] migrate from Docker action to composite, using `gh` CLI
     - Replace manual curl/REST API calls with `gh pr view` and `gh pr checkout`.
     - Use `--force-with-lease` for push, with pushRemote-style push when the PR branch name matches the default branch.
     - Remove Dockerfile.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,68 +10,52 @@ if [ -z "$GITHUB_TOKEN" ]; then
 	exit 1
 fi
 
-URI=https://api.github.com
-API_HEADER="Accept: application/vnd.github.v3+json"
-AUTH_HEADER="Authorization: token $GITHUB_TOKEN"
+pr_json=$(gh pr view "$PR_NUMBER" --repo "$GITHUB_REPOSITORY" --json baseRefName,headRefName,isCrossRepository,maintainerCanModify,mergeable)
 
-pr_resp=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-          "${URI}/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER")
+BASE_BRANCH=$(echo "$pr_json" | jq -r .baseRefName)
+HEAD_BRANCH=$(echo "$pr_json" | jq -r .headRefName)
+IS_CROSS_REPO=$(echo "$pr_json" | jq -r .isCrossRepository)
+CAN_MODIFY=$(echo "$pr_json" | jq -r .maintainerCanModify)
+MERGEABLE=$(echo "$pr_json" | jq -r .mergeable)
 
-BASE_REPO=$(echo "$pr_resp" | jq -r .base.repo.full_name)
-BASE_BRANCH=$(echo "$pr_resp" | jq -r .base.ref)
-
-# echo "API response: $pr_resp"
-
-if [ "$(echo "$pr_resp" | jq -r .rebaseable)" != "true" ]; then
-	>&2 echo 'GitHub doesn‘t think that the PR is rebaseable! it probably has merge conflicts'
+if [ "$MERGEABLE" = "CONFLICTING" ]; then
+	>&2 echo 'PR has merge conflicts and cannot be rebased!'
 	exit 1
 fi
 
-if [[ -z "$BASE_BRANCH" ]]; then
+if [ "$MERGEABLE" = "UNKNOWN" ]; then
+	>&2 echo 'GitHub has not yet determined if the PR is mergeable. Try again shortly.'
+	exit 1
+fi
+
+if [ -z "$BASE_BRANCH" ]; then
 	>&2 echo "Cannot get base branch information for PR #${PR_NUMBER}!"
 	exit 1
 fi
 
-HEAD_REPO=$(echo "$pr_resp" | jq -r .head.repo.full_name)
-HEAD_BRANCH=$(echo "$pr_resp" | jq -r .head.ref)
-REBASEABLE=$(echo "$pr_resp" | jq -r .rebaseable)
-CAN_MODIFY=$(echo "$pr_resp" | jq -r .maintainer_can_modify)
-IS_FORK=$(echo "$pr_resp" | jq -r .base.repo.fork)
-
-if [ $REBASEABLE != true ]; then
-	>&2 echo 'Branch is not rebaseable; has merge conflicts'
-	exit 1
-fi
-if [ $CAN_MODIFY != true ] && [ $IS_FORK = true ]; then
-	>&2 echo 'PR is a fork, and does not allow edits'
+if [ "$CAN_MODIFY" != "true" ] && [ "$IS_CROSS_REPO" = "true" ]; then
+	>&2 echo 'PR is from a fork, and does not allow edits'
 	exit 1
 fi
 
 echo "Base branch for PR #${PR_NUMBER} is ${BASE_BRANCH}"
 
-# avoids weird `fatal: unsafe repository ('/github/workspace' is owned by someone else)` error
-# See https://github.com/actions/checkout/issues/766 for motivation.
-git config --global --add safe.directory /github/workspace
-
-git remote add --no-tags pr_source https://x-access-token:$GITHUB_TOKEN@github.com/${HEAD_REPO}.git
-
-git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git.git
 git config --global user.email 'action@github.com'
 git config --global user.name 'GitHub Action'
 
 set -o xtrace
 
-# make sure branches are up-to-date
-git fetch origin $BASE_BRANCH
-git fetch pr_source $HEAD_BRANCH
+# Check out the PR branch using gh
+gh pr checkout "$PR_NUMBER"
 
-# ensure that if the head branch and base branch have the same name, nothing breaks
-git checkout origin/$BASE_BRANCH
-git branch -D $HEAD_BRANCH || echo "$HEAD_BRANCH did not exist already"
+# Get the local branch name (may differ from HEAD_BRANCH if names collide with base)
+LOCAL_BRANCH=$(git symbolic-ref HEAD --short)
 
-# do the rebase
-git checkout -b $HEAD_BRANCH pr_source/$HEAD_BRANCH
-MERGE_COUNT=$(git log --oneline origin/$BASE_BRANCH..pr_source/$HEAD_BRANCH --merges | wc -l | tr -d '[:space:]')
+# Make sure base branch is up-to-date
+git fetch origin "$BASE_BRANCH"
+
+# Count merge commits
+MERGE_COUNT=$(git log --oneline "origin/$BASE_BRANCH..HEAD" --merges | wc -l | tr -d '[:space:]')
 if [ $? -ne 0 ]; then
 	>&2 echo 'Unable to count merge commits'
 	exit 1;
@@ -80,14 +64,23 @@ if [ $MERGE_COUNT -eq 0 ]; then
 	echo 'No merge commits found, yay!'
 	exit 0
 fi
+
+# Do the rebase
 if [ "$GITHUB_REPOSITORY" = 'tc39/ecma262' ]; then
-	git rebase -f origin/$BASE_BRANCH
+	git rebase -f "origin/$BASE_BRANCH"
 else
-	git rebase origin/$BASE_BRANCH --committer-date-is-author-date
+	git rebase "origin/$BASE_BRANCH" --committer-date-is-author-date
 fi
 
-# push back
-git push pr_source HEAD:$HEAD_BRANCH --force-with-lease
+# Push back
+if [ "$HEAD_BRANCH" = "$BASE_BRANCH" ]; then
+	# When the PR branch name matches the default branch, use pushRemote-style push
+	PUSH_REMOTE=$(git config --get "branch.${LOCAL_BRANCH}.pushRemote" 2>/dev/null || git config --get "branch.${LOCAL_BRANCH}.remote")
+	MERGE_REF=$(git config --get "branch.${LOCAL_BRANCH}.merge" | awk -F / '{print $NF}')
+	git push "${PUSH_REMOTE}" "+HEAD:${MERGE_REF}"
+else
+	git push --force-with-lease
+fi
 
 >&2 echo "${MERGE_COUNT} merge commit(s) found; PR branch rebased."
 exit 1
